df4d6acab2c2a9ee9601b7fe8b5ce02c3b19c579
Joseph Cottingham <joseph.cottingham@protonmail.com>
2021-04-07 20:25:16 -0400

    Users can join chat and send messages

--- public/index.js
+++ public/index.js
@@ -1,20 +1,45 @@
-// index.js
+//index.js
+var currentUser = null;
 $(document).ready(()=>{
-    const socket = io.connect();
-  
-    $('#create-user-btn').click((e)=>{
-      e.preventDefault();
-      let username = $('#username-input').val();
-      if(username.length > 0){
-        //Emit to the server the new user
-        socket.emit('newUser', username);
-        $('.username-form').remove();
-      }
-    });
+  const socket = io.connect();
 
-    //socket listeners
-    socket.on('newUser', (username) => {
-        console.log(`${username} has joined the chat!`);
-    })
-  
+  $('#create-user-btn').click((e)=>{
+    e.preventDefault();
+    if($('#username-input').val().length > 0){
+      currentUser = $('#username-input').val();
+      socket.emit('newUser', currentUser);
+      $('.username-form').remove();
+      // Have the main page visible
+      $('.main-container').css('display', 'flex');
+    }
+  });
+
+  $('#send-chat-btn').click((e)=>{
+    e.preventDefault();
+    msg = $('#chat-input').val();
+    if(msg.length > 0){
+      console.log('msg sent');
+      socket.emit('newMsg', {user:currentUser,msg:msg});
+      $('#chat-input').val("");
+    }
+  });
+
+  //socket listeners
+  socket.on('newUser', (username) => {
+    console.log(`${username} has joined the chat`);
+    // Add the new user to the online users div
+    $('.users-online').append(`<div class="user-online">${username}</div>`);
   })
+  socket.on('newMsg', (newMsg) => {
+    console.log(newMsg);
+    // Add the new user to the online users div
+    $('.message-container').append(`
+    <div class="message">
+      <p class="message-user">${newMsg.user}: </p>
+      <p class="message-text">${newMsg.msg}</p>
+    </div>
+  `);
+  })
+  
+
+})
--- sockets/chat.js
+++ sockets/chat.js
@@ -6,4 +6,11 @@ module.exports = (io, socket) => {
       console.log(`${username} has joined the chat!`);
       io.emit("newUser", username);
     })
-  }
+
+    //Listen for new messages
+  socket.on('newMsg', (data) => {
+    // Send that data back to ALL clients
+    console.log(`${data.user}: ${data.msg}`)
+    io.emit('newMsg', data);
+  })
+}
